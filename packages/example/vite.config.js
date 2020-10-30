46ee60b678525d821d6fc0d4de6d3adb9249f984
Dylan Vann <dylan@dylanvann.com>
2020-10-30 04:06:11 -0400

    chore: upgrade dependencies, switch example to vite

---
+++ packages/example/vite.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+import reactPlugin from 'vite-plugin-react'
+
+/**
+ * @type { import('vite').UserConfig }
+ */
+const config = {
+  jsx: 'react',
+  plugins: [reactPlugin]
+}
+
+export default config
